ff70d303c50556a4d619a7092791cd9b04a6fe5c
gitudrebel <gitudrebel94@gmail.com>
2020-07-19 16:13:59 +0530

    Add basic structure

--- lib/main.dart
+++ lib/main.dart
@@ -6,22 +6,20 @@ void main() => runApp(TodoApp());
 class TodoApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return CupertinoApp(
-      localizationsDelegates: [DefaultMaterialLocalizations.delegate],
-      builder: (context,int){
-        return  CupertinoPageScaffold(
-          navigationBar: CupertinoNavigationBar(
-            middle: Text('Me Title'),
-          ),
-          child: SafeArea(
-            child: Container(
-              color: Colors.blue,
-              child: Text("Hellow",textDirection: TextDirection.ltr,),
-            ),
-          ),
-        );
-      },
-      color: Colors.blue,
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(),
+        body:ListScreenWidget()
+      ),
+    );
+  }
+}
+
+class ListScreenWidget extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text("Hello world"),
     );
   }
 }
