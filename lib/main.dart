355b054a9ed474b615fbfa6cdaa8b265b0397532
gitudrebel <gitudrebel94@gmail.com>
2020-08-03 23:53:30 +0530

    Add FAB

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_todo/Task.dart';
+import 'package:flutter_todo/add_task.dart';
 import 'package:flutter_todo/task_detail.dart';
 
 void main() => runApp(TodoApp());
@@ -8,31 +9,44 @@ void main() => runApp(TodoApp());
 class TodoApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(appBar: AppBar(), body: ListScreenWidget()),
-    );
+    return MaterialApp(home: ListScreenWidget());
   }
 }
 
 class ListScreenWidget extends StatelessWidget {
-  final List<Task> taskList=getDummyTaskList();
+  final List<Task> taskList = getDummyTaskList();
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-        child: ListView.builder(
-            itemCount: taskList.length,
-            itemBuilder: (context, index) {
-              return ListTile(
-                onTap: (){
-                  print("${taskList[index].title}");
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => TaskDetailStateful()),
-                  );
-                },
-                title: Text("${taskList[index].title}"),
-                subtitle: Text("${taskList[index].status}"),
-              );
-            }));
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+                builder: (context) => AddTask()),
+          );
+        },
+      ),
+      appBar: AppBar(),
+      body: Container(
+          child: ListView.builder(
+              itemCount: taskList.length,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  onTap: () {
+                    print("${taskList[index].title}");
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => TaskDetailStateful()),
+                    );
+                  },
+                  title: Text("${taskList[index].title}"),
+                  subtitle: Text("${taskList[index].status}"),
+                );
+              })),
+    );
   }
 }
