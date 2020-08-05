315ed327fe1a940ee74c2c8618c108dea1368313
gitudrebel <gitudrebel94@gmail.com>
2020-08-05 19:28:22 +0530

    Add task UI

--- lib/add_task.dart
+++ lib/add_task.dart
@@ -6,8 +6,48 @@ class AddTask extends StatefulWidget {
 }
 
 class _AddTaskState extends State<AddTask> {
+
+  final _formKey=GlobalKey<FormState>();
+
+  final controllerTitle = TextEditingController();
+  final controllerDesc = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(),
+      body: Form(
+        key: _formKey,
+        child: Column(
+          children: <Widget>[
+            TextFormField(
+              controller: controllerTitle,
+              decoration: InputDecoration(hintText: 'Task title'),
+              validator: (value){
+                if(value.isEmpty)
+                  {
+                    return 'Please enter a title';
+                  }
+                return null;
+              },
+            ),
+            TextFormField(
+              controller: controllerDesc,
+              minLines: 10,
+              maxLines: 10,
+              decoration: InputDecoration(hintText: 'Task description'),
+            ),
+            RaisedButton(
+              onPressed: () {
+                if(_formKey.currentState.validate()) {
+                  print('Task title is ${controllerTitle.text}');
+                }
+              },
+              child: Text('Add Task'),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
