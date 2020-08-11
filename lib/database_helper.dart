efd964396e65cc2bfb2b1824fefdd49bbca9978d
gitudrebel <gitudrebel94@gmail.com>
2020-08-11 21:51:29 +0530

    DB helper

---
+++ lib/database_helper.dart
@@ -0,0 +1,41 @@
+import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart';
+
+class DatabaseHelper {
+  static DatabaseHelper _instance = new DatabaseHelper._internal();
+  Database _database;
+
+  factory DatabaseHelper() {
+    return _instance;
+  }
+
+  DatabaseHelper._internal();
+
+  Future<Database> get database async {
+
+    if(_database!=null)
+      return _database;
+
+    _database = await openDatabase(
+      join(await getDatabasesPath(), 'todo_database.db'),
+      onCreate: (db, version) {
+        return db.execute(
+          "CREATE TABLE tasks(id INTEGER PRIMARY KEY,user_id INTEGER, title TEXT, desc TEXT,status TEXT)",
+        );
+      },
+      version: 1,
+    );
+
+    return _database;
+
+  }
+
+  Future<int> addTask(Map task) async {
+    _database=await database;
+    int row = await _database.insert('tasks', task);
+    return row;
+  }
+
+
+
+}
