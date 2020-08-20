a60dd86933d500b979b6e325a48c38774437aac6
gitudrebel <gitudrebel94@gmail.com>
2020-08-21 00:30:25 +0530

    Update UI after deleting a Task

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_todo/Task.dart';
+import 'package:flutter_todo/task.dart';
 import 'package:flutter_todo/task_detail.dart';
 
 void main() => runApp(TodoApp());
@@ -27,7 +26,7 @@ class ListScreenWidget extends StatelessWidget {
                   print("${taskList[index].title}");
                   Navigator.push(
                     context,
-                    MaterialPageRoute(builder: (context) => TaskDetailStateful()),
+                    MaterialPageRoute(builder: (context) => TaskDetail()),
                   );
                 },
                 title: Text("${taskList[index].title}"),
